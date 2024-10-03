30004cf5b96eee5d1bd2dc0a0d3df5d8a505e30f
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-03 15:24:14 -0400

    update more styles on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,22 @@
-import { ReactNode } from 'react'
+'use client';
+
+import { ReactNode, useState } from 'react'
 import Link from 'next/link'
 
 function DashboardLayout({children}: {children: ReactNode}) {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
-      <nav className='flex flex-col md:flex-row justify-between p-4 gap-4 bg-slate-600 text-gray-400 shadow-md'>
-        <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>Home</Link>
-        <div className='flex flex-col md:flex-row gap-4'>
-          <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>My Items</Link>
-          <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>Borrowed Items</Link>
+      <nav className='flex flex-col md:flex-row justify-between p-4 bg-slate-600 text-gray-400 shadow-md'>
+        <div className='flex flex-row-reverse justify-between items-center'>
+          <button onClick={() => setOpen(!isOpen)} className='md:hidden hover:text-orange-200 transition-all duration-300'>| | |</button>
+          <Link href='#' className='p-2 hover:text-orange-200 transition-all duration-300'>Home</Link>
+        </div>
+        <div className={`flex flex-col md:flex-row overflow-hidden transition-all duration-300 ${isOpen ? 'h-32' : 'h-0'} md:h-auto`}>
+          <Link href='#' className='p-2 hover:text-orange-200 transition-all duration-300'>My Items</Link>
+          <Link href='#' className='p-2 hover:text-orange-200 transition-all duration-300'>Borrowed Items</Link>
+          <Link href='#' className='p-2 border-t border-gray-400 md:border-none md:ml-5 hover:text-orange-200 transition-all duration-300'>Signout</Link>
         </div>
       </nav>
       {children}
