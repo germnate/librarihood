4add52bf88c971fa7fb4ff3bf75293eda3965385
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-03 15:03:26 -0400

    update a few styles

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function DashboardLayout({children}: {children: ReactNode}) {
   return (
     <>
-      <nav className='flex flex-col md:flex-row justify-between p-4 gap-4 bg-slate-600 text-gray-400'>
+      <nav className='flex flex-col md:flex-row justify-between p-4 gap-4 bg-slate-600 text-gray-400 shadow-md'>
         <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>Home</Link>
         <div className='flex flex-col md:flex-row gap-4'>
           <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>My Items</Link>
