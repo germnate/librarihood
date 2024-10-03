1afa3b4bbb80317fe3cc17c21d23087c0e751a2a
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-03 03:45:29 -0400

    add dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+import Link from 'next/link'
+
+function DashboardLayout({children}: {children: ReactNode}) {
+  return (
+    <>
+      <nav className='flex flex-col md:flex-row justify-between p-4 gap-4 bg-slate-600 text-gray-400'>
+        <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>Home</Link>
+        <div className='flex flex-col md:flex-row gap-4'>
+          <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>My Items</Link>
+          <Link href='#' className='border border-gray-400 p-2 rounded hover:text-orange-200 hover:bg-gray-400 transition-all duration-300'>Borrowed Items</Link>
+        </div>
+      </nav>
+      {children}
+    </>
+  )
+}
+
+export default DashboardLayout
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+ return <div>sup</div> 
+}
+
+export default Dashboard
