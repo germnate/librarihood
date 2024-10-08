2572e558c63037952e5f29cf3f23c3210affb6d7
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-08 16:25:12 -0400

    handle next-auth signin callback
    * credentials are now checked to make sure that they are coming from
    the 'credentials' provider.
    * Github authentication makes sure it is coming from github but also
    after being authenticated it creates a user in the database if one
    is not created.
    * Github users are given a throw away password because pocketbase.io
    won't allow users without passwords to be created. However, the password
    is not used for authentication when the provider is one other than
    credentials.

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,8 +2,54 @@ import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { getAuthenticUser } from '../../../lib/user'
-import { User } from '../../../types/user'
-import type { SessionUser } from '@/app/types/session-user'
+import { User as pbUser } from '../../../types/user'
+import pb from '../../../lib/db'
+import { RecordModel } from 'pocketbase'
+import { SessionUser } from '@/app/types/session-user'
+import { User, Account, Profile } from 'next-auth'
+
+interface Params {
+  user: User;
+  account: Account | null;
+  profile?: Profile | undefined;
+  email?: { verificationRequest?: boolean | undefined; } | undefined;
+  credentials?: Record<string, unknown> | undefined;
+}
+
+async function authorizeCreds(credentials: { username: string; password: string }) {
+  const user: pbUser | null = await getAuthenticUser(credentials?.username || '', credentials?.password || '')
+  if (user) {
+    return user
+  } else {
+    return null
+  }
+}
+
+async function checkSignInCredentials(credentials: { username: string; password: string }) {
+  const user = await authorizeCreds(credentials)
+  return !!user;
+}
+
+async function findOrCreate(params: Params) {
+  const existingUser: Array<RecordModel> = await pb.collection('users').getFullList({
+    filter: `providerId="${params.user.id}"`
+  })
+  if (existingUser.length) {
+    return true;
+  } else {
+    await pb.collection('users').create({
+      email: params.user.email,
+      name: params.user.name,
+      providerId: params.user.id,
+      // the passwords are required to save a user in pocketbase.io
+      // However these passwords cannot be used since we first check the provider
+      // and in order to enter a password the provider must be "credentials" not "github" or some other provider.
+      password: 'temporary',
+      passwordConfirm: 'temporary',
+    })
+    return true;
+  }
+}
 
 export const options: NextAuthOptions = {
   providers: [
@@ -25,12 +71,8 @@ export const options: NextAuthOptions = {
         }
       },
       async authorize(credentials) {
-        const user: User | null = await getAuthenticUser(credentials?.username || '', credentials?.password || '')
-        if (user) {
-          return user
-        } else {
-          return null
-        }
+        if (!credentials) return null;
+        return authorizeCreds(credentials)
       }
     })
   ],
@@ -45,6 +87,18 @@ export const options: NextAuthOptions = {
       const user: SessionUser = session?.user || {}
       user.id = token.id as string
       return session;
+    },
+    async signIn(params: Params) {
+      if (params?.account?.provider === 'credentials') {
+        if (params?.credentials && typeof params.credentials?.username === 'string' && typeof params.credentials?.password === 'string') {
+          const credentials = params.credentials as { username: string; password: string }
+          return checkSignInCredentials(credentials)
+        }
+      }
+      if (params?.account?.provider === 'github') {
+        return findOrCreate(params)
+      }
+      return false;
     }
   }
 }
