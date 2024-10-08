b537c1d844e2f3c0d0d535c121550ebf5a72c4a1
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-08 12:53:29 -0400

    retrieve your book list for the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,15 +4,25 @@ import { options } from "../api/auth/[...nextauth]/options";
 import pb from '../lib/db'
 import { BookList } from "./BookList";
 import { Book } from "../models/book";
+import { RecordModel } from "pocketbase";
+import { SessionUser } from "../types/session-user";
 
 async function Dashboard() {
   const session = await getServerSession(options);
-  const collection = await pb.collection('Books').getFullList();
-  const books = Book.collection(collection)
-  console.log(books)
+  const user: SessionUser | undefined = session?.user
+  let books: Array<RecordModel> = []
+  if(user?.id) {
+    console.log('user', session?.user)
+    books = await pb.collection('Books').getFullList({
+      filter: `userId = '${user.id}'`,
+    })
+    console.log('records', books)
+  }
+
+  const bookCollection = Book.collection(books)
   return (
     <div className=''>
-      <BookList books={books} />
+      <BookList books={bookCollection} />
     </div>
   )
 }
