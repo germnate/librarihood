e69956b20b1ad7509d05be3a01f77096b7e45cea
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-08 20:54:13 -0400

    add book pages

---
+++ src/app/books/new/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+export default function NewBookPage() {
+    return (
+        <div>
+            <div className='flex flex-col p-4 border'>
+                <form className='flex flex-col gap-4'>
+                    <div className='flex flex-col'>
+                        <label>Title</label>
+                        <input type='text' name='title' className='p-2 border' required />
+                    </div>               
+                    <div className='flex flex-col'>
+                        <label>Author</label>
+                        <input type='text' name='author' className='p-2 border' />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label>ISBN</label>
+                        <input type='text' name='isbn' className='p-2 border' />
+                    </div>
+                    <button type='submit' className='border py-2 bg-green-100 hover:bg-green-200'>Create</button>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/books/page.tsx
@@ -0,0 +1,4 @@
+import Link from "next/link"
+export default function ManageBooks() {
+    return <Link href="/books/new">Add a book</Link>
+}
