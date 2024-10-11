3e19fe61a0941b1d4920a34036511801b54545b5
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-11 10:32:43 -0400

    change req type to NextRequest
    * this is used for the new App Router

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest } from 'next/server';
 import pb from '../../lib/db'
 
-async function handler(req: Request, res: Response) {
+async function handler(req: NextRequest) {
   try {
     const submissionData = await req.json()
-    const result = await pb.collection('users').create(submissionData)
+    await pb.collection('users').create(submissionData)
     return new Response(JSON.stringify({ success: true }), { status: 200 })
-  } catch (error: any) {
+  } catch (error) {
     const status = error.status || 500;
     const message = error.message || 'Internal server error';
     const details = error.data || {}
