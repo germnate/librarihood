f78722d29eecb7c0950c19eaf179aa4db53b005e
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-15 14:06:53 -0400

    make fetchUtil
    * I kept forgetting to wrap the body in JSON.stringify
    and I got tired of putting in the method and headers when they were always the same.

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -5,4 +5,14 @@ function getDetailedError(data: SignUpErrorDetails): Array<Array<string>> | null
   return Object.keys(data).map(key => [key, data[key].message])
 }
 
-export { getDetailedError }
+function fetchUtil({ url, body, method = 'POST' }: { url: string, body: object, method?: string }) {
+  return fetch(url, {
+    method,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+}
+
+export { getDetailedError, fetchUtil }
