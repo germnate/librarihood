3b3ed16136f9047efdf46fe14859b0635b968b5d
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-15 18:57:20 -0400

    update cover image mime types

---
+++ pb_migrations/1729024987_updated_Books.js
@@ -0,0 +1,51 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("t3ndhiatfxd5xpf")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ihdd1p92",
+    "name": "cover",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/png",
+        "image/jpeg"
+      ],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("t3ndhiatfxd5xpf")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ihdd1p92",
+    "name": "cover",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
