62e53dbe3fba11b493e12872e70d61713b8f6c6b
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-15 21:08:47 -0400

    fix uploading issues

--- src/app/books/new/NewBookPage.tsx
+++ src/app/books/new/NewBookPage.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import { fetchUtil } from "@/app/utils";
-import { useRef } from 'react'
-// import { useRouter } from "next/navigation";
+import { useRef, useState } from 'react'
 
 export default function NewBookPage({ userId }: { userId: string | undefined }) {
-  // const router = useRouter()
+  const [coverType, setCoverType] = useState('file');
   const fileInputRef = useRef<HTMLInputElement>(null)
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!userId) throw new Error('No User Error!')
     const formData = new FormData(e.currentTarget);
-    if (fileInputRef?.current?.files) {
+    formData.delete('select')
+    if (coverType === 'file' && fileInputRef?.current?.files?.length) {
       formData.append('cover', fileInputRef.current.files[0])
+      formData.delete('coverUrl')
     }
     formData.append('userId', userId)
     const res = await fetch('/api/books/new', {
@@ -28,12 +29,18 @@ export default function NewBookPage({ userId }: { userId: string | undefined }) 
     // There is no 200 response from the server. In the meantime,
     // the below option is working as expected.
     window.location.href = `/books/${bookId}` // and same issue with /books/:id
-    // router.push(`/books/${bookId}`)
+  }
+
+  const setFile = () => {
+    setCoverType('file');
+  }
+  const setUrl = () => {
+    setCoverType('url');
   }
 
   return (
     <div>
-      <div className='flex flex-col p-4 border'>
+      <div className='flex flex-col p-4'>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
           <div className='flex flex-col'>
             <label>Title</label>
@@ -47,10 +54,16 @@ export default function NewBookPage({ userId }: { userId: string | undefined }) 
             <label>ISBN</label>
             <input type='text' name='isbn' className='p-2 border' />
           </div>
-          <div className='flex flex-col'>
-            <label>Cover</label>
-            <input ref={fileInputRef} type='file' name='cover' className='p-2 border' />
+          <div className='flex gap-2'>
+            <input id='file-label' type='radio' name='select' onChange={setFile} value={coverType} checked={coverType === 'file'} />
+            <label htmlFor='file-label'>File</label>
           </div>
+          <div className='flex gap-2'>
+            <input id='url-label' type='radio' name='select' onChange={setUrl} value={coverType} checked={coverType === 'url'} />
+            <label htmlFor='url-label'>Url</label>
+          </div>
+          <input ref={fileInputRef} type='file' accept='.jpg, .png, .webp' className={coverType !== 'file' ? 'hidden' : 'p-2 border'} />
+          <input name='coverUrl' className={coverType === 'file' ? 'hidden' : 'p-2 border'} />
           <button type='submit' className='border py-2 bg-green-100 hover:bg-green-200'>
             Create
           </button>
