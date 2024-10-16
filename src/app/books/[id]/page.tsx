73c59e5b3dc4b4702fb1b66f10e7d4d4aeaa0904
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-15 21:09:01 -0400

    display coverUrl

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -6,7 +6,7 @@ export default async function ShowBook({ params }: { params: { id: string } }) {
     const book = await getBook(params.id)
     const url = pb.files.getUrl(book, book.cover, { thumb: '100x250' })
     return <div className='m-4'>
-        <img src={url} alt='Book Cover' />
+        <img src={url || book.coverUrl} alt='Book Cover' />
         <h1 className='text-5xl'>{book.title}</h1>
         <h2 className='text-2xl'>{book.author}</h2>
         <span>{book.isbn}</span>
