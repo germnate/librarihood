098e7c01ca6d328cd22b51991e2bd48ce496433c
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-15 21:07:15 -0400

    update books table to have coverUrl and image types

---
+++ pb_migrations/1729033330_updated_Books.js
@@ -0,0 +1,30 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("t3ndhiatfxd5xpf")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "a6l9nypn",
+    "name": "coverUrl",
+    "type": "url",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "exceptDomains": [],
+      "onlyDomains": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("t3ndhiatfxd5xpf")
+
+  // remove
+  collection.schema.removeField("a6l9nypn")
+
+  return dao.saveCollection(collection)
+})
