05b169d9163c62e53beef2c6ff3f90fcf65d7577
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-24 10:48:55 -0400

    notify user when search turns up nothing

--- src/app/books/new/IsbnSearch.tsx
+++ src/app/books/new/IsbnSearch.tsx
@@ -9,17 +9,30 @@ import { GoogleJson, Item } from '@/app/types/google-books-api';
 export function IsbnSearch({ userId }: { userId: string | undefined }) {
   const [isbn, setIsbn] = useState('')
   const [json, setJson] = useState<GoogleJson | any>({})
+  const [showNoResults, setShowNoResults] = useState(false);
   const handleSearch = async (e: React.FormEvent) => {
     e.preventDefault();
     const response = await fetch(`/api/books/fetch?isbn=${isbn}`)
-    const data = await response.json();
-    console.log(data)
-    setJson(data);
+    const json = await response.json();
+    console.log(json)
+    setJson(json);
+    if (json.data.totalItems === 0) {
+      flash();
+    }
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsbn(e.target.value)
   }
+
+  function flash() {
+    console.log('setting flash');
+    setShowNoResults(true);
+    setTimeout(() => {
+      setShowNoResults(false);
+    }, 3000)
+  }
+
   return (
     <div className='flex flex-col p-4 items-center font-sans'>
       <form className='flex items-center justify-center w-full md:w-3/4 xl:w-1/2' >
@@ -41,6 +54,7 @@ export function IsbnSearch({ userId }: { userId: string | undefined }) {
       </form >
       <div className='w-full'>
         Search Results: {json?.data?.totalItems || 0}
+        <div className={`${showNoResults ? 'opacity-100 ' : 'opacity-0 '}px-4 py-2 bg-libraryGray text-gray-400 text-center rounded-full transition-all duration-300`}>No Results</div>
         {json?.error ? <p className='text-red-600'>Error: {json?.error}</p> : null}
         {
           json?.data ?
