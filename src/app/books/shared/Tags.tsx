ab47e8e9150864345ce82156a5efab3cd16f8715
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-24 11:17:52 -0400

    fix null bug

--- src/app/books/shared/Tags.tsx
+++ src/app/books/shared/Tags.tsx
@@ -4,7 +4,7 @@ import { Tag } from './Tag'
 export function Tags({ book }: { book: BooksRecord }) {
     return (
         <div className='flex flex-wrap gap-2'>
-            {book?.tags?.map((tag: string) => {
+            {book?.tags?.map?.((tag: string) => {
                 return <Tag key={tag} tag={tag} />
             })}
         </div>
