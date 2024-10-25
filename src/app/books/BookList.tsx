984aea0bf777885db413b83abaa617e4eb5dbdc9
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-25 15:15:34 -0400

    add client side fetch of books
    * search isn't hooked up yet
    * for some reason useEffect is running twice and the first call is causing an autocancellation from pocketbase. This may be fine, but I don't understand why useEffect is running twice. An empty dependency array should make it only run on mount.

--- src/app/books/BookList.tsx
+++ src/app/books/BookList.tsx
@@ -1,18 +1,37 @@
 'use client';
 
 import { Card } from "./Card"
-import type { Book } from "../types/book"
-import Link from "next/link"
-import { AddBookLink } from "./AddBookLink"
 import { BooksRecord } from "../../../pocketbase-types"
 import { useNavBarStatus } from "../NavBarStatusContext"
+import { fetchUtil } from "../utils";
+import { useEffect, useState } from "react";
 
-function BookList({ books }: { books: Array<BooksRecord> }) {
+
+
+function BookList({ userId }: { userId: string | undefined }) {
   const { navBarStatus } = useNavBarStatus();
+  const [books, setBooks] = useState<Array<BooksRecord>>([]);
+  async function getBooks(userId: string | undefined) {
+    try {
+      console.log('calling getBooks...')
+      const res = await fetchUtil({ url: '/api/books/index', body: { userId, tokens: [] } })
+      const data = await res.json();
+      console.log(data);
+      if (data.books) setBooks(data.books)
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    getBooks(userId);
+  }, [])
   return (
     <>
       <div className={`${navBarStatus ? 'ml-20 md:ml-32' : '-translate-x-full'} absolute z-[15] bottom-0 top-0 right-0 left-0 px-2 py-5 bg-black opacity-[80%] transition-all duration-300`}>
         <input className='w-full lg:w-1/2 rounded-full px-2' type='search' name='search' placeholder='Search...' />
+        <button onClick={() => getBooks(userId)} className='bg-green-500 px-2 mt-2 py-1 text-gray-700 rounded-full'>Search</button>
       </div>
       <div className='flex flex-col items-center md:items-start md:flex-row flex-wrap mx-4 gap-7'>
         {books.map(book => <Card key={book.id} book={book} />)}
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,30 +1,15 @@
-import Link from "next/link"
 import { BookList } from "./BookList";
-import { Book } from "../models/book";
 import { SessionUser } from "../types/session-user";
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
-import { RecordModel } from "pocketbase";
-import pb from '@/app/lib/db'
-import { AddBookLink } from "./AddBookLink";
-import { BooksRecord } from "../../../pocketbase-types";
 
 export default async function ManageBooks() {
     const session = await getServerSession(options);
     const user: SessionUser | undefined = session?.user
-    let books: Array<BooksRecord> = []
-    if (user?.id) {
-        books = await pb.collection('books').getFullList({
-            filter: `userId = '${user.id}'`,
-            cache: 'no-store'
-        })
-    }
 
     return (
         <div className='pt-4'>
-            {!books.length
-                ? <div className='flex justify-center'><AddBookLink /></div>
-                : <BookList books={books} />}
+            <BookList userId={user?.id} />
         </div>
     )
 
